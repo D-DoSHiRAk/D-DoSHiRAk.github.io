88574ceb9c12e4108ac71cc9d076b2a5d4fd5ba1
D-Doshirak <islyam1013@gmail.com>
2025-02-04 18:34:38 +0400

    test1.3.1

--- vars.js
+++ vars.js
@@ -18,10 +18,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 const parts = value.split(",");
                 let i = 0;
                 parts.forEach((value1, key1) => {
-                    const container = document.createElement("div");
+                    const container = document.createElement("button");
                     let x = 150*i;
                     let y = 100*i;
-                    container.innerHTML = '<a1 class="button" id="button" style="position: absolute; top: ' + y + 'px; left: ' + x + 'px;">' + value1 + '</a1>';
+                    container.innerHTML = '<button class="button" id="button" style="position: absolute; top: ' + y + 'px; left: ' + x + 'px;">' + value1 + '</button>';
                     document.body.appendChild(container);
                 });
             }else{
