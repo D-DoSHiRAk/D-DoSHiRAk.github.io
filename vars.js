0aee36adea124f0e7d91c4aacbce30b8a0990055
D-Doshirak <islyam1013@gmail.com>
2025-02-04 18:32:02 +0400

    test1.3

--- vars.js
+++ vars.js
@@ -1,7 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     const params = new URLSearchParams(window.location.search);
 
+    const trueVars = ["products"];
+
     params.forEach((value, key) => {
-        document.getElementById(key).textContent = value;
+        let checker = false;
+
+        trueVars.forEach((value1, key1) =>{
+            if(key1 == key){
+                checker = true;
+                return;
+            }
+        });
+
+        if(checker){
+            if(key=="products"){
+                const parts = value.split(",");
+                let i = 0;
+                parts.forEach((value1, key1) => {
+                    const container = document.createElement("div");
+                    let x = 150*i;
+                    let y = 100*i;
+                    container.innerHTML = '<a1 class="button" id="button" style="position: absolute; top: ' + y + 'px; left: ' + x + 'px;">' + value1 + '</a1>';
+                    document.body.appendChild(container);
+                });
+            }else{
+                document.getElementById(key).textContent = value;
+            }
+            //id
+        }
     });
 });
