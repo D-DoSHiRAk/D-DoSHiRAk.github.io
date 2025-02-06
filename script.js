fcf134d951f49e339fba2b6b88f57e314842344c
D-Doshirak <islyam1013@gmail.com>
2025-02-06 22:47:47 +0400

    test1.4.2

--- script.js
+++ script.js
@@ -10,18 +10,33 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.appendChild(container);
     }
 
+    function createRect(id, x, y, width, height, col){
+        const rectangle = document.createElement("button");
+        rectangle.className = "button";
+        rectangle.id = id;
+        rectangle.style.position = "absolute";
+        rectangle.style.width = width;
+        rectangle.style.height = height;
+        rectangle.style.backgroundColor = col;
+        rectangle.style.transform = "translate(" + x + ", " + y + ")";
+        rectangle.style.borderRadius = "8px";
+        document.body.appendChild(rectangle);
+    }
+
     function createProductCards(value){
         const parts = value.split(",");
 
         parts.forEach((value1, index) => {
-            let x = -150;
-            let y = 200*index + 800;
-            createElement("text", "text", value1+index, x-20, y-14, "____________________________", 20);
-            createElement("text", "text", value1+index, x, y, value1, 48);
-            createElement("text", "text", value1+index, x + 150, y + 64, value1, 20);
-            createElement("button", "button", value1+index, x, y + 65, "Купить", 32);
+            let x = -170;
+            let y = 200*index + 700;
+            createRect(value1+index, x - 35 + "px", y + "px", "500px", "180px", 'lightgray')
+            createElement("text", "text", value1+index, x, y + 15, value1, 48);
+            createElement("text", "text", value1+index, x + 180, y + 15, value1, 20);
+            createElement("button", "button", value1+index, x, y + 85, "Купить", 32);
         });
     }
+
+    createRect("background-rect", "-50%", "62%", "100%", "1080px", '#1b232a');
 
     //window.location.search
     //products=VPN,Server,Test
