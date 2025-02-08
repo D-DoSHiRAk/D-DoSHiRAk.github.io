ed72836397640de83c9e129fa64e73b9e6b2927d
D-Doshirak <islyam1013@gmail.com>
2025-02-08 12:47:06 +0400

    test1.4.7

--- test.js
+++ test.js
@@ -10,35 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.appendChild(container);
     }
 
-    function fetchGoogleSheetJSON(url) {
+    async function fetchGoogleSheetJSON(url) {
         try {
-            createElement('text', 'text', `log2`, 0, 600, "2", 32);
-            const response = UrlFetchApp.fetch(url);
-            createElement('text', 'text', `log3`, 100, 700, "3", 32);
-            if (!response.ok){
-                createElement('text', 'text', `err1`, 0, 700, "error1", 32);
-                throw new Error(`Ошибка: ${response.status}`);
-            }
-            const data = response.json();
-            createElement('text', 'text', `log4`, 0, 800, "4", 32);
+            const response = await fetch(url);
+            if (!response.ok) throw new Error(`Ошибка: ${response.status}`);
+            const data = await response.json();
+            console.log("Данные из Google Sheets:", data);
             return data;
         } catch (error) {
-            createElement('text', 'text', `err2`, 200, 700, "error2", 32);
+            console.error("Ошибка при загрузке данных:", error);
             return null;
         }
     }
 
-    function main() {
-        const url = "https://script.google.com/macros/s/AKfycbzUnbxsQ7ZZnZ0bx8D5Sfisvv3d6DeIGDCOiMJ-xaCuYq8s1I2NiQb-l_-Xt3I92TASlQ/exec";
-        createElement('text', 'text', `log1`, 0, 500, "1", 32);
-        const data = fetchGoogleSheetJSON(url);
-        createElement('text', 'text', `log5`, 0, 900, "5", 32);
-        if (data && Array.isArray(data)) {
-            data.forEach((item, index) => {
-                createElement('text', 'text', `test-${index}`, 0, 100 + index * 40, item.name, 32);
-            });
-        }
-    }
+    const url = "https://script.google.com/macros/s/AKfycbycX2Kr8Hy21Ik0IFgRCQqMHrk9DpVSU52aOHEW0MamkTEMKqXa1N6ZWNIIr7PIWzG7WA/exec";
+    const data = fetchGoogleSheetJSON(url);
 
-    main();
+    createElement('text', 'text', `test`, 0, 900, data, 32);
 });
